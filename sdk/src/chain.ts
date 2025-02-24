dcb8f88670d670777d426d1cf07cb55858db5a7d
Seun Lanlege <seun@polytope.technology>
2025-02-24 02:03:58 +0000

    some more docs

--- sdk/src/chain.ts
+++ sdk/src/chain.ts
@@ -1,5 +1,11 @@
 export * from "./chains/evm"
 
+/**
+ * Interface representing a chain.
+ */
 export interface IChain {
+	/*
+	 * Returns the current timestamp of the chain.
+	 */
 	timestamp(): Promise<bigint>
 }
