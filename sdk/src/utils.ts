a2094f43ca1b2bab5222d6cd82b5be320287956a
Seun Lanlege <seun@polytope.technology>
2025-02-24 02:05:18 +0000

    substrate chains

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -6,6 +6,22 @@ export function sleep(ms?: number) {
 	return new Promise((resolve) => setTimeout(resolve, ms || 5_000))
 }
 
+/**
+ * Checks if the given state machine ID represents an EVM chain.
+ * @param stateMachineId The state machine ID to check.
+ */
 export function isEvmChain(stateMachineId: string): boolean {
 	return stateMachineId.startsWith("EVM")
+}
+
+/**
+ * Checks if the given state machine ID represents a Substrate chain.
+ * @param stateMachineId The state machine ID to check.
+ */
+export function isSubstrateChain(stateMachineId: string): boolean {
+	return (
+		stateMachineId.startsWith("POLKADOT") ||
+		stateMachineId.startsWith("KUSAMA") ||
+		stateMachineId.startsWith("SUBSTRATE")
+	)
 }
