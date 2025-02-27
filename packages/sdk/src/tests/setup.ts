cb26883ab198cc40263a60b8cfde94dcdd18ccf4
Desmond Obisi <desmond@polytope.technology>
2025-02-27 16:04:26 +0100

    chore: fix ci issues

--- packages/sdk/src/tests/setup.ts
+++ packages/sdk/src/tests/setup.ts
@@ -3,6 +3,5 @@ process.env.NODE_ENV = "test"
 import * as dotenv from "dotenv"
 import * as path from "path"
 
-const root = path.resolve().split("/")
-root.pop()
-dotenv.config({ path: path.resolve(root.join("/") + "/.env") })
+const root = process.cwd()
+dotenv.config({ path: path.resolve(root, ".env") })
