abe4fd2daf702a2bd4c4f3dda8f6f24ac9bbc6fb
royvardhan <royvardhan.official@gmail.com>
2025-04-17 00:03:58 +0530

    fix: build

--- packages/intent-filler/src/index.ts
+++ packages/intent-filler/src/index.ts
@@ -1,13 +1,6 @@
-// Export your package's public API here
 export const version = "0.1.0"
 
 export { IntentFiller } from "./core/filler"
 export { EventMonitor } from "./core/event-monitor"
 export { BasicFiller } from "./strategies/basic"
 export { ConfirmationPolicy } from "./config/confirmation-policy"
-export { ChainConfig } from "./types"
-export { Order } from "./types"
-export { FillOptions } from "./types"
-export { CancelOptions } from "./types"
-export { NewDeployment } from "./types"
-export { RequestBody } from "./types"
