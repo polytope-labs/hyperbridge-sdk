36998bb4a9a3c3c197e2a41610a0a8c298d07105
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-05-15 17:36:08 +0100

    chore: add wasm file to source code

--- packages/sdk/src/utils/ckb-mmr-wasm/build.sh
+++ packages/sdk/src/utils/ckb-mmr-wasm/build.sh
@@ -5,3 +5,5 @@ pwd
 
 wasm-pack build -t web -d dist/web --release --mode="normal" --no-default-features --out-name=web $1
 wasm-pack build -t nodejs -d dist/node --release --mode="normal" --no-default-features --out-name=node $1
+
+rm ./dist/**/.gitignore ./dist/**/README.md ./dist/**/package.json
