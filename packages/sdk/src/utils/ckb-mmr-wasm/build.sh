8ec627aae962de1cdff85691277eb82bee659ac2
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-05-15 16:07:18 +0100

    chore: remove unnecesary code

--- packages/sdk/src/utils/ckb-mmr-wasm/build.sh
+++ packages/sdk/src/utils/ckb-mmr-wasm/build.sh
@@ -5,6 +5,3 @@ pwd
 
 wasm-pack build -t web -d dist/web --release --mode="normal" --no-default-features --out-name=web $1
 wasm-pack build -t nodejs -d dist/node --release --mode="normal" --no-default-features --out-name=node $1
-
-# rm dist/bundler/.gitignore dist/bundler/package.json dist/bundler/README.md
-# rm dist/node/.gitignore dist/node/package.json dist/node/README.md
