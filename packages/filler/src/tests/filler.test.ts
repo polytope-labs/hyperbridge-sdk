3ef487134b1b3bf73da82b2275efecc8180a7990
royvardhan <royvardhan.official@gmail.com>
2025-06-04 18:50:45 +0530

    fix

--- packages/filler/src/tests/filler.test.ts
+++ packages/filler/src/tests/filler.test.ts
@@ -75,7 +75,7 @@ describe.sequential("Basic", () => {
 		})
 	})
 
-	it.only("Should listen, place order, fill order, and check if filled at the source chain", async () => {
+	it("Should listen, place order, fill order, and check if filled at the source chain", async () => {
 		const {
 			bscIntentGateway,
 			gnosisChiadoIntentGateway,
