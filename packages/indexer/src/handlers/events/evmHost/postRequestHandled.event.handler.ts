14ac9a018c5b2a1832ac3ee21b0d0b8fdd4ebb7e
🎴 <69737492+vimoppa@users.noreply.github.com>
2025-08-04 20:51:43 +0100

    chore: add volume tracking for token transfers
    Signed-off-by: 🎴 <69737492+vimoppa@users.noreply.github.com>

--- packages/indexer/src/handlers/events/evmHost/postRequestHandled.event.handler.ts
+++ packages/indexer/src/handlers/events/evmHost/postRequestHandled.event.handler.ts
@@ -6,6 +6,9 @@ import { getHostStateMachine } from "@/utils/substrate.helpers"
 import { getBlockTimestamp } from "@/utils/rpc.helpers"
 import stringify from "safe-stable-stringify"
 import { wrap } from "@/utils/event.utils"
+import { ERC6160Ext20Abi__factory } from "@/configs/src/types/contracts"
+import PriceHelper from "@/utils/price.helpers"
+import { VolumeService } from "@/services/volume.service"
 
 /**
  * Handles the PostRequestHandled event from Hyperbridge
@@ -38,6 +41,25 @@ export const handlePostRequestHandledEvent = wrap(async (event: PostRequestHandl
 			status: Status.DESTINATION,
 			transactionHash,
 		})
+
+		if (transaction &&  transaction.logs) {
+			for (const log of transaction.logs) {
+			  // check if the topic includes Transfer(address, address, uint256)
+			  if (!log.topics.includes("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")) {
+					continue
+				}
+
+			  const contract = ERC6160Ext20Abi__factory.connect(log.address, api)
+
+				const amount = BigInt(log.data)
+				const symbol = await contract.symbol()
+				const decimals = await contract.decimals()
+
+				const price = await PriceHelper.getTokenPriceInUSDCoingecko(symbol, amount, decimals)
+
+				await VolumeService.updateVolume(`Transfer.${symbol}`, price.amountValueInUSD, blockTimestamp)
+			}
+		}
 	} catch (error) {
 		console.error(`Error handling PostRequestHandled event: ${stringify(error)}`)
 	}
--- packages/indexer/src/handlers/events/evmHost/postRequestTimeoutHandled.event.handler.ts
+++ packages/indexer/src/handlers/events/evmHost/postRequestTimeoutHandled.event.handler.ts
@@ -6,6 +6,9 @@ import { wrap } from "@/utils/event.utils"
 import { getBlockTimestamp } from "@/utils/rpc.helpers"
 import { getHostStateMachine } from "@/utils/substrate.helpers"
 import stringify from "safe-stable-stringify"
+import { ERC6160Ext20Abi__factory } from "@/configs/src/types/contracts"
+import PriceHelper from "@/utils/price.helpers"
+import { VolumeService } from "@/services/volume.service"
 
 /**
  * Handles the PostRequestTimeoutHandled event
@@ -38,6 +41,25 @@ export const handlePostRequestTimeoutHandledEvent = wrap(async (event: PostReque
 			status: Status.TIMED_OUT,
 			transactionHash,
 		})
+
+		if (transaction &&  transaction.logs) {
+			for (const log of transaction.logs) {
+			  // check if the topic includes Transfer(address, address, uint256)
+			  if (!log.topics.includes("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")) {
+					continue
+				}
+
+			  const contract = ERC6160Ext20Abi__factory.connect(log.address, api)
+
+				const amount = BigInt(log.data)
+				const symbol = await contract.symbol()
+				const decimals = await contract.decimals()
+
+				const price = await PriceHelper.getTokenPriceInUSDCoingecko(symbol, amount, decimals)
+
+				await VolumeService.updateVolume(`Transfer.${symbol}`, price.amountValueInUSD, blockTimestamp)
+			}
+		}
 	} catch (error) {
 		logger.error(`Error updating handling post request timeout: ${stringify(error)}`)
 	}
