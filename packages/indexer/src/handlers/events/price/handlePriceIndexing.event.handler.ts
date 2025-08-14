9afe4d4592e7bc89e4844c497ee3a1476c72726d
vimoppa <69737492+vimoppa@users.noreply.github.com>
2025-08-14 12:55:49 +0100

    chore: add price feed schema and event handler

---
+++ packages/indexer/src/handlers/events/price/handlePriceIndexing.event.handler.ts
@@ -0,0 +1,20 @@
+import stringify from "safe-stable-stringify"
+import { getBlockTimestamp } from "@/utils/rpc.helpers"
+import { getHostStateMachine } from "@/utils/substrate.helpers"
+import { wrap } from "@/utils/event.utils"
+import { TransferLog } from "@/configs/src/types/abi-interfaces/ERC6160Ext20Abi"
+
+/**
+ * Handle Price Indexing for all registered tokens on a chain when significant events occur
+ */
+export const handlePriceIndexing = wrap(async (transfer: TransferLog): Promise<void> => {
+	logger.info(`Price Update Event triggered: ${stringify(transfer)}`)
+
+	const { blockNumber, block, transactionHash } = transfer
+	const chain = getHostStateMachine(chainId)
+	const timestamp = await getBlockTimestamp(block.hash, chain)
+
+	logger.info(`Updating prices for chain: ${chain}, block: ${blockNumber}, timestamp: ${timestamp}`)
+
+	logger.info(`Price update completed for chain: ${chain}`)
+})
